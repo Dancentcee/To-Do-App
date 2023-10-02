f3565a00e4d6a1ecd6318cef30b2637d143da026
Daniel Okafor <dancentcee38@gmail.com>
2023-10-02 13:59:11 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-12_x
+    pkgs.nodejs
+    pkgs.yarn global add vercel
+    pkgs.yarn global add vercel
+    pkgs.nodejs-16_x
+    pkgs.npm install -g vercel
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server  
+  ];
+}
